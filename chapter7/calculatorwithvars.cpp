03c8a6e5d38ca575e4a2da0a200bd988952ec79f
AbsoluteAbsolver <64785006+AbsoluteAbsolver@users.noreply.github.com>
2020-10-26 22:09:50 +0100

    added constant variables

--- chapter7/calculatorwithvars.cpp
+++ chapter7/calculatorwithvars.cpp
@@ -3,6 +3,17 @@
 	calculator08buggy.cpp
 
 	grammer:
+	statement:
+		expression
+		declaration
+		assignment
+
+	declaration:
+		let name = expression
+
+	assignment
+		name = expression
+
 	expression:
 		term
 		term + expression
@@ -13,7 +24,8 @@
 		term / primary
 	primary:
 		number
-		'(' + expression + ')'
+		name
+		'(' expression ')'
 		'sqrt(' expression ')'
 		- primary
 		+ primary
@@ -37,30 +49,34 @@ struct Token {
 
 class Token_stream {
 public:
-	Token_stream() :full(false), buffer(0) { }
+	//Token_stream() :full(false), buffer(0) { }
 
 	Token get();
-	void unget(Token t) { buffer = t; full = true; }
-
-	void ignore(char);
+	void putback(Token t) { buffer.push_back(t); }
+	void ignore(char c);
 private:
-	bool full;
-	Token buffer;
+	vector<Token> buffer;
 };
 
 const char let = 'L';
+const char con = 'C';
 const char quit = 'Q';
 const char print = ';';
 const char number = '8';
 const char name = 'a';
 const char square = 's';
 const string declkey = "let";
+const string constkey = "const";
 const string quitkey = "quit";
 const string squarekey = "sqrt";
 
 Token Token_stream::get()
 {
-	if (full) { full = false; return buffer; }
+	if (!buffer.empty()) {
+		Token t = buffer.back();
+		buffer.pop_back();
+		return t;
+	}
 	char ch;
 	cin >> ch;
 	switch (ch) {
@@ -94,10 +110,11 @@ Token Token_stream::get()
 		if (isalpha(ch)) {
 			string s;
 			s += ch;
-			while (cin.get(ch) && (isalpha(ch) || isdigit(ch))) s += ch; //2nd error compilor won'catch
+			while (cin.get(ch) && (isalpha(ch) || isdigit(ch) || ch == '_')) s += ch; //2nd error compilor won'catch
 			cin.unget();
 			if (s == squarekey) return Token(square); 
 			if (s == declkey) return Token(let); //another error? yes
+			if (s == constkey) return Token(con);
 			if (s == quitkey) return Token(quit); //another error
 			return Token(name, s);
 		}
@@ -107,11 +124,10 @@ Token Token_stream::get()
 
 void Token_stream::ignore(char c)
 {
-	if (full && c == buffer.kind) {
-		full = false;
-		return;
-	}
-	full = false;
+	while (!buffer.empty() && buffer.back().kind != c)
+		buffer.pop_back();
+	// Buffer contains a c kind token
+	if (!buffer.empty()) return;
 
 	char ch;
 	while (cin >> ch)
@@ -122,7 +138,9 @@ class Variable {
 public:
 	string name;
 	double value;
-	Variable(string n, double v) :name(n), value(v) { }
+	bool constant;
+	//const bool isconst;
+	Variable(string n, double v, bool c) :name(n), value(v), constant(c){ }
 };
 
 vector<Variable> names;
@@ -138,6 +156,9 @@ void set_value(string s, double d)
 {
 	for (int i = 0; i <= names.size(); ++i)
 		if (names[i].name == s) {
+			if (names[i].constant) {
+				error("variable you are trying to set is constant");
+			}
 			names[i].value = d;
 			return;
 		}
@@ -202,7 +223,7 @@ double term()
 		break;
 		}
 		default:
-			ts.unget(t);
+			ts.putback(t);
 			return left;
 		}
 	}
@@ -221,22 +242,41 @@ double expression()
 			left -= term();
 			break;
 		default:
-			ts.unget(t);
+			ts.putback(t);
 			return left;
 		}
 	}
 }
 
+double assignment()
+{
+	// We get there by knowing that a name and an '=' come next.
+	Token t = ts.get();
+	string var_name = t.name;
+	if (!is_declared(var_name)) error(var_name, " has not been declared");
+	ts.get(); // Get rid of the '='
+	double d = expression();
+	set_value(var_name, d);
+	return d;
+}
+
 double declaration()
 {
-	Token t = ts.get();
-	if (t.kind != 'a') error("name expected in declaration");
-	string name = t.name;
+	Token t1 = ts.get(); //let or const
+	Token t2 = ts.get(); //name of var
+	if (t2.kind != name) error("name expected in declaration");
+	string name = t2.name;
 	if (is_declared(name)) error(name, " declared twice");
-	Token t2 = ts.get();
-	if (t2.kind != '=') error("= missing in declaration of ", name);
+	Token t3 = ts.get();
+	if (t3.kind != '=') error("= missing in declaration of ", name);
 	double d = expression();
-	names.push_back(Variable(name, d));
+	switch(t1.kind) {
+	case let:
+		names.push_back(Variable(name, d, false));
+	case con:
+		names.push_back(Variable(name, d, true));
+
+	}
 	return d;
 }
 
@@ -244,10 +284,27 @@ double statement()
 {
 	Token t = ts.get();
 	switch (t.kind) {
-	case let:
+	case let: case con:
+		ts.putback(t);
 		return declaration();
+	case name:
+	{
+		Token t2 = ts.get();
+		// Whatever t2 is, we have to rollback
+		ts.putback(t2);
+		ts.putback(t);
+		if (t2.kind == '=') {
+			return assignment();
+		}
+		// We could move the putback(t) inside the if-statement and let this
+		// fallback to the default case. I have my doubts that something like
+		// that won't give us problems on the future. The current solution also
+		// will be problematic if something else than an expression is
+		// introduced as a statement.
+		return expression();
+	}
 	default:
-		ts.unget(t);
+		ts.putback(t);
 		return expression();
 	}
 }
@@ -267,7 +324,7 @@ void calculate()
 		Token t = ts.get();
 		while (t.kind == print) t = ts.get();
 		if (t.kind == quit) return;
-		ts.unget(t);
+		ts.putback(t);
 		cout << result << statement() << endl;
 	}
 	catch (exception& e) {
